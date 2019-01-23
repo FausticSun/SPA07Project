a333c5dccbcf1d26aaea7450d10c6e4019e83d0f
crizyli <liyufei6666@gmail.com>
2019-01-23 13:49:18 +0800

    Define VarTable and ProcTable API
    add abstractions for VarTable and ProcTable

--- src/spa/src/PKB.h
+++ src/spa/src/PKB.h
@@ -4,17 +4,37 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include<list>
 
 using namespace std;
 typedef short PROC;
 
 class TNode;
 
-class VarTable;  // no need to #include "VarTable.h" as all I need is pointer
+class VarTable {  // no need to #include "VarTable.h" as all I need is pointer
+public:
+	list<string> varList;
+	int numOfVars;
+	VarTable();
+	int add(string s);
+	bool contains(string s);
+	string getAll();
+};
+
+class ProcTable {
+public:
+	list<string> procList;
+	int numOfProcs;
+	ProcTable();
+	int add(string s);
+	bool contains(string s);
+	string getAll();
+};
 
 class PKB {
 public:
 	static VarTable* varTable; 
+	static ProcTable* procTable;
 	static int setProcToAST(PROC p, TNode* r);
 	static TNode* getRootAST (PROC p);
 
