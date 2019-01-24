d9482f41b77b2c61223fec5e752c6e894facf2de
elroyhaw <elroyhaw@gmail.com>
2019-01-24 23:56:25 +0800

    Implement TNode API

--- src/spa/src/TNode.cpp
+++ src/spa/src/TNode.cpp
@@ -1,1 +1,13 @@
 #include "TNode.h"
+
+
+TNode::TNode(TNodeType type, string name) {
+	this->type = type;
+	this->name = name;
+}
+
+TNode::~TNode() {}
+
+void TNode::addChildren(TNode* child) {
+	children.push_back(child);
+}
