61d0d9a9b4258dfdc41c17214928532de66df6f4
elroyhaw <elroyhaw@gmail.com>
2019-01-24 23:56:02 +0800

    Define TNode API

--- src/spa/src/TNode.h
+++ src/spa/src/TNode.h
@@ -1,5 +1,43 @@
-class TNode
-{
-public:
+#pragma once
 
+#include <vector>
+#include <string>
+
+using std::vector;
+using std::string;
+
+enum class TNodeType
+{
+	Program,
+	Procedure,
+	If,
+	Then,
+	Else,
+	While,
+	Variable,
+	Constant,
+	StmtLst,
+	Assign,
+	Call,
+	SemiColon,
+	CurlyBraceOpen,
+	CurlyBraceClose,
+	Equals,
+	Plus,
+	Minus,
+	Times,
+	Divide,
+	Mod
+};
+
+class TNode {
+public:
+	TNode(TNodeType, string);
+	~TNode();
+	void addChildren(TNode*);
+
+private:
+	TNodeType type;
+	string name;
+	vector<TNode*> children;
 };
