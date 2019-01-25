eed8fa2801599dcc231f66399303d0a184ba9386
florafong97 <e0042072@u.nus.edu>
2019-01-26 02:39:52 +0800

    Implement parser for procedure command

--- src/spa/src/Parser.cpp
+++ src/spa/src/Parser.cpp
@@ -3,6 +3,9 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <sstream>
+#include <algorithm>
+#include <iterator>
 
 using namespace std;
 
@@ -16,9 +19,48 @@ Parser::Parser(string input) {
 }
 
 int Parser::Parse(string input) {
-	cout << input;
+
+	vector<string> tokens;
+	istringstream iss(input);
+	copy(istream_iterator<string>(iss),
+		istream_iterator<string>(),
+		back_inserter(tokens));
+
+	//identifying different types of statement
+	if (find(tokens.begin(), tokens.end(), "procedure") != tokens.end()) {
+		parseProcedure(tokens);
+	}
+
+	if (find(tokens.begin(), tokens.end(), "=") != tokens.end()) {
+		//parseAssignment(tokens);
+	}
 
 	return 0;
+
+}
+
+void Parser::parseProcedure(vector<string> tokens) {
+	tokenQueue.push(make_pair(TokenType::Keyword, "procedure"));
+	tokenQueue.push(make_pair(TokenType::Identifier, tokens[1]));
+	tokenQueue.push(make_pair(TokenType::Operator, "{"));
+
+
+	queue<pair<TokenType, string>> q = tokenQueue;
+	while (!q.empty())
+	{
+		string type;
+		TokenType t = q.front().first;
+		switch (t) {
+		case TokenType::Keyword: type = "keyword"; break;
+		case TokenType::Identifier: type = "identifier"; break;
+		case TokenType::Operator: type = "operator"; break;
+
+		}
+		cout << type << " " << q.front().second << endl;
+		q.pop();
+	}
+	std::cout << std::endl;
+
 }
 
 
--- src/spa/src/Parser.h
+++ src/spa/src/Parser.h
@@ -1,11 +1,26 @@
 #pragma once
 
 #include<string>
+#include<queue>
+#include<utility>
+
 using namespace std;
+
+enum class TokenType {
+	Keyword,
+	Identifier,
+	Separator,
+	Operator,
+	Number
+};
 
 class Parser {
 	string code;
 public:
 	Parser(string input);
 	int Parse(string);
+
+private:
+	queue<pair<TokenType, string>> tokenQueue;
+	void parseProcedure(vector<string>);
 };
