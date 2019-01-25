4072f9aae6df127270bfc230b4386f8e2ad6983e
elroyhaw <elroyhaw@gmail.com>
2019-01-25 23:57:36 +0800

    Add unit tests for TNode

---
+++ src/unit_testing/src/TestTNode.cpp
@@ -0,0 +1,33 @@
+#include "TNode.h"
+
+#include "catch.hpp"
+
+SCENARIO("Construction of TNode") {
+
+	TNode* tNode = new TNode(TNodeType::Procedure, "child");
+
+	SECTION("Type of TNode") {
+		REQUIRE(tNode->getType() == TNodeType::Procedure);
+	}
+
+	SECTION("Name of TNode") {
+		REQUIRE(tNode->getName() == "child");
+	}
+
+	SECTION("TNode has no children during construction") {
+		REQUIRE(tNode->getChildren().size() == 0);
+	}
+
+}
+
+SCENARIO("Children to TNode") {
+
+	TNode* childTNode = new TNode(TNodeType::Procedure, "child");
+	TNode* parentTNode = new TNode(TNodeType::Procedure, "parent");
+
+	SECTION("Adding one child to TNode") {
+		parentTNode->addChildren(childTNode);
+		REQUIRE(parentTNode->getChildren().front() == childTNode);
+	}
+
+}
