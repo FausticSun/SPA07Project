c576da433d7cf9e98f2e324cb941936dd0d392c6
florafong97 <e0042072@u.nus.edu>
2019-01-28 15:53:06 +0800

    Added Lexer layer to Parser

--- src/spa/src/Parser.h
+++ src/spa/src/Parser.h
@@ -1,36 +1,3 @@
 #pragma once
-
-#include<string>
-#include<queue>
-#include<utility>
-
 using namespace std;
-
-enum class TokenType {
-	Keyword,
-	Identifier,
-	Separator,
-	Operator,
-	Literal,
-	Fail
-};
-
-class Parser {
-	string code;
-public:
-	Parser(string input);
-	int Parse(string);
-
-private:
-	queue<pair<TokenType, string>> tokenQueue;
-	void parseProcedure(vector<string>);
-	void parseAssignment(vector<string>);
-	void pushToTokenQueue(vector<string>);
-	vector<string> arrangeSemiColon(vector<string>);
-	bool isIdentifier(string);
-	bool isSeparator(string);
-	bool isOperator(string);
-	bool isLiteral(string);
-	bool onlyContainDigits(string);
-
-};
+int Parse();
