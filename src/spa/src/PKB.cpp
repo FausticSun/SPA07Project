d491bbfae927face29100b5e391483c6b4645339
FausticSun <FausticSun@gmail.com>
2019-02-06 04:48:22 +0800

    Remove previous implementation of PKB

--- src/spa/src/PKB.cpp
+++ src/spa/src/PKB.cpp
@@ -1,13 +1,1 @@
-#include <cstdio>
-#include <iostream>
-#include <string>
-#include <vector>
-
-using namespace std;
-
 #include "PKB.h"
-#include "TNode.h"
-
-int PKB::setProcToAST(PROC /*p*/, TNode * /*r*/) { return 0; }
-
-TNode *PKB::getRootAST(PROC /*p*/) { return nullptr; }
--- src/spa/src/PKB.h
+++ src/spa/src/PKB.h
@@ -5,16 +5,4 @@
 #include <string>
 #include <vector>
 
-using namespace std;
-using PROC = short;
-
-class TNode;
-
-class VarTable; // no need to #include "VarTable.h" as all I need is pointer
-
-class PKB {
-public:
-  static VarTable *varTable;
-  static int setProcToAST(PROC p, TNode *r);
-  static TNode *getRootAST(PROC p);
-};
+class PKB {};
