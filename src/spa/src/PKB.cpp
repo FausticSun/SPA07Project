8e9761854e016e516895e6dc2d0fa302dd87a834
FausticSun <FausticSun@gmail.com>
2019-02-06 05:30:48 +0800

    Implement PKB

--- src/spa/src/PKB.cpp
+++ src/spa/src/PKB.cpp
@@ -1,1 +1,14 @@
 #include "PKB.h"
+PKB::PKB() { this->setPKB(); }
+void PKB::setPKB(const std::set<std::string> &varTable,
+                 const std::set<std::string> &procTable,
+                 const std::set<Relation> &relTable) {
+  this->varTable = varTable;
+  this->procTable = procTable;
+  this->relTable = relTable;
+}
+const std::set<std::string> PKB::getVarTable() const { return this->varTable; }
+const std::set<std::string> PKB::getProcTable() const {
+  return this->procTable;
+}
+const std::set<Relation> PKB::getRelTable() const { return this->relTable; }
--- src/spa/src/PKB.h
+++ src/spa/src/PKB.h
@@ -1,8 +1,21 @@
 #pragma once
 
-#include <cstdio>
-#include <iostream>
+#include "Relation.h"
+#include <set>
 #include <string>
-#include <vector>
 
-class PKB {};
+class PKB {
+private:
+  std::set<std::string> varTable;
+  std::set<std::string> procTable;
+  std::set<Relation> relTable;
+
+public:
+  PKB();
+  void setPKB(const std::set<std::string> &varTable = {},
+              const std::set<std::string> &procTable = {},
+              const std::set<Relation> &relTable = {});
+  const std::set<std::string> getVarTable() const;
+  const std::set<std::string> getProcTable() const;
+  const std::set<Relation> getRelTable() const;
+};
