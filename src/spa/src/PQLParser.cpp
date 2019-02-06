92ba23768125bab6e59de657e66f2ea6a2bc277f
FausticSun <FausticSun@gmail.com>
2019-02-07 02:42:09 +0800

    Format code

--- src/spa/src/PQLParser.cpp
+++ src/spa/src/PQLParser.cpp
@@ -1,132 +1,103 @@
 #include "PQLParser.h"
+#include <algorithm>
+#include <queue>
 #include <string.h>
 #include <string>
 #include <vector>
-#include <queue>
-#include <algorithm>
 
 using namespace std;
 
-PQLParser::PQLParser(string input)
-{
-	Tokenize(input);
+PQLParser::PQLParser(string input) { Tokenize(input); }
+
+void PQLParser::Tokenize(string input) {
+  vector<string> token = vectorize(input);
+  while (0 < token.size()) {
+    if (find(token.begin(), token.end(), "procedure") != token.end()) {
+      tokenizeProcedure(token);
+      token.erase(token.begin());
+      token.erase(token.begin());
+    }
+
+    if (find(token.begin(), token.end(), "variable") != token.end()) {
+      tokenizeVariable(token);
+      token.erase(token.begin());
+      token.erase(token.begin());
+    }
+
+    if (find(token.begin(), token.end(), "select") != token.end()) {
+      tokenizeSelect(token);
+      token.erase(token.begin());
+      token.erase(token.begin());
+    }
+
+    if (find(token.begin(), token.end(), "Modifies") != token.end()) {
+      tokenizeModifies(token);
+      token.erase(token.begin());
+      token.erase(token.begin());
+    }
+
+    if (find(token.begin(), token.end(), "Uses") != token.end()) {
+      tokenizeUses(token);
+    }
+
+    if (find(token.begin(), token.end(), "Parent*") != token.end()) {
+      tokenizeParentS(token);
+    } else if (find(token.begin(), token.end(), "Parent") != token.end()) {
+      tokenizeParent(token);
+    }
+
+    if (find(token.begin(), token.end(), "Follows*") != token.end()) {
+      tokenizeFollowsS(token);
+    } else if (find(token.begin(), token.end(), "Follows") != token.end()) {
+      tokenizeFollows(token);
+    }
+  }
 }
 
-void PQLParser::Tokenize(string input)
-{
-	vector<string> token = vectorize(input);
-	while (0 < token.size()) {
-	if (find(token.begin(), token.end(), "procedure") != token.end()) {
-		tokenizeProcedure(token);
-		token.erase(token.begin());
-		token.erase(token.begin());
-	}
-
-	if (find(token.begin(), token.end(), "variable") != token.end()) {
-		tokenizeVariable(token);
-		token.erase(token.begin());
-		token.erase(token.begin());
-	}
-
-	if (find(token.begin(), token.end(), "select") != token.end()) {
-		tokenizeSelect(token);
-		token.erase(token.begin());
-		token.erase(token.begin());
-	}
-
-	if (find(token.begin(), token.end(), "Modifies") != token.end()) {
-		tokenizeModifies(token);
-		token.erase(token.begin());
-		token.erase(token.begin());
-	}
-
-	if (find(token.begin(), token.end(), "Uses") != token.end()) {
-		tokenizeUses(token);
-	}
-
-	if (find(token.begin(), token.end(), "Parent*") != token.end()) {
-		tokenizeParentS(token);
-	}
-	else if (find(token.begin(), token.end(), "Parent") != token.end()) {
-		tokenizeParent(token);
-	}
-
-	if (find(token.begin(), token.end(), "Follows*") != token.end()) {
-		tokenizeFollowsS(token);
-	}
-	else if (find(token.begin(), token.end(), "Follows") != token.end()) {
-		tokenizeFollows(token);
-	}
+vector<string> PQLParser::vectorize(string input) {
+  vector<string> tokens;
+  char *p;
+  char *temp = (char *)input.c_str();
+  p = strtok(temp, " ;");
+  while (p) {
+    tokens.push_back(p);
+    p = strtok(NULL, " ;");
+  }
+  return tokens;
 }
 
+queue<pair<TokenType, string>> PQLParser::getDeclarationQueue() {
+  return declarationQueue;
 }
 
-vector<string> PQLParser::vectorize(string input)
-{
-	vector<string> tokens;
-	char *p;
-	char *temp = (char*)input.c_str();
-	p = strtok(temp, " ;");
-	while (p) {
-		tokens.push_back(p);
-		p = strtok(NULL, " ;");
-	}
-	return tokens;
+queue<pair<TokenType, string>> PQLParser::getSelectQueue() {
+  return selectQueue;
 }
 
-queue<pair<TokenType, string>> PQLParser::getDeclarationQueue()
-{
-	return declarationQueue;
+void PQLParser::tokenizeVariable(vector<string> token) {
+  declarationQueue.push(make_pair(TokenType::Keyword, "variable"));
+  declarationQueue.push(make_pair(TokenType::Identifier, token[1]));
 }
 
-queue<pair<TokenType, string>> PQLParser::getSelectQueue()
-{
-	return selectQueue;
-}
-
-
-void PQLParser::tokenizeVariable(vector<string> token)
-{
-	declarationQueue.push(make_pair(TokenType::Keyword, "variable"));
-	declarationQueue.push(make_pair(TokenType::Identifier, token[1]));
-}
-
-void PQLParser::tokenizeProcedure(vector<string> token)
-{
-	declarationQueue.push(make_pair(TokenType::Keyword, "procedure"));
-	declarationQueue.push(make_pair(TokenType::Identifier, token[1]));
+void PQLParser::tokenizeProcedure(vector<string> token) {
+  declarationQueue.push(make_pair(TokenType::Keyword, "procedure"));
+  declarationQueue.push(make_pair(TokenType::Identifier, token[1]));
 }
 
 void PQLParser::tokenizeSelect(vector<string> token) {
-	selectQueue.push(make_pair(TokenType::Keyword, "selection"));
-	selectQueue.push(make_pair(TokenType::Identifier, token[1]));
+  selectQueue.push(make_pair(TokenType::Keyword, "selection"));
+  selectQueue.push(make_pair(TokenType::Identifier, token[1]));
 }
 
-void PQLParser::tokenizePattern(vector<string>)
-{
-}
+void PQLParser::tokenizePattern(vector<string>) {}
 
-void PQLParser::tokenizeFollows(vector<string>)
-{
-}
+void PQLParser::tokenizeFollows(vector<string>) {}
 
-void PQLParser::tokenizeFollowsS(vector<string>)
-{
-}
+void PQLParser::tokenizeFollowsS(vector<string>) {}
 
-void PQLParser::tokenizeParent(vector<string>)
-{
-}
+void PQLParser::tokenizeParent(vector<string>) {}
 
-void PQLParser::tokenizeParentS(vector<string>)
-{
-}
-void PQLParser::tokenizeUses(vector<string>)
-{
-}
+void PQLParser::tokenizeParentS(vector<string>) {}
+void PQLParser::tokenizeUses(vector<string>) {}
 
-void PQLParser::tokenizeModifies(vector<string>)
-{
-}
-
-
+void PQLParser::tokenizeModifies(vector<string>) {}
--- src/spa/src/TNode.cpp
+++ src/spa/src/TNode.cpp
@@ -1,29 +1,19 @@
 #include "TNode.h"
 
 TNode::TNode(TNodeType type, string name, int statementNumber) {
-	this->type = type;
-	this->name = name;
-	this->statementNumber = statementNumber;
+  this->type = type;
+  this->name = name;
+  this->statementNumber = statementNumber;
 }
 
 TNode::~TNode() {}
 
-TNodeType TNode::getType() {
-	return type;
-}
+TNodeType TNode::getType() { return type; }
 
-string TNode::getName() {
-	return name;
-}
+string TNode::getName() { return name; }
 
-int TNode::getStatementNumber() {
-	return statementNumber;
-}
+int TNode::getStatementNumber() { return statementNumber; }
 
-vector<TNode*> TNode::getChildren() {
-	return children;
-}
+vector<TNode *> TNode::getChildren() { return children; }
 
-void TNode::setChildren(vector<TNode*> children) {
-	this->children = children;
-}
+void TNode::setChildren(vector<TNode *> children) { this->children = children; }
