5b298679c65b1ccc64e8ddd69214a4a470827c9a
FausticSun <FausticSun@gmail.com>
2019-02-07 02:24:17 +0800

    Attempt to fix unit tests

--- src/unit_testing/src/TestPqlEvaluator.cpp
+++ src/unit_testing/src/TestPqlEvaluator.cpp
@@ -5,18 +5,13 @@
 using namespace std;
 
 SCENARIO("Selecting variable command") {
-	string PQL = "variable v; select v;";
-	PqlEvaluator pe;
-	WHEN("PKB is contructed") {
-		
-	}
+  string PQL = "variable v; select v;";
+  PqlEvaluator pe;
+  WHEN("PKB is contructed") {}
 }
 
 SCENARIO("Selecting procedure command") {
-	string PQL = "procedure p;select p;";
-	PqlEvaluator pe();
-	WHEN("PKB is contructed") {
-
-	}
-
+  string PQL = "procedure p;select p;";
+  PqlEvaluator pe();
+  WHEN("PKB is contructed") {}
 }
