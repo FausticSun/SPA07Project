9fbb313eb49e4be1f8f4b894faa25183c6499a27
elroyhaw <elroyhaw@gmail.com>
2019-02-07 10:39:20 +0800

    Update unit test for AST builder

--- src/unit_testing/src/TestASTBuilder.cpp
+++ src/unit_testing/src/TestASTBuilder.cpp
@@ -237,3 +237,79 @@ SCENARIO("Procedure and if statements only") {
 	}
 
 }
+
+SCENARIO("Procedure and while statements with multiple conditions") {
+
+	Parser parser;
+	queue<Token*> tokenQueue;
+	tokenQueue.push(new Token(TokenType::Procedure, "procedure", 1));
+	tokenQueue.push(new Token(TokenType::Identifier, "Example", 1));
+	tokenQueue.push(new Token(TokenType::Separator, "{", 1));
+	tokenQueue.push(new Token(TokenType::While, "while", 2));
+	tokenQueue.push(new Token(TokenType::OpenParenthesis, "(", 2));
+	tokenQueue.push(new Token(TokenType::OpenParenthesis, "(", 2));
+	tokenQueue.push(new Token(TokenType::Identifier, "x", 2));
+	tokenQueue.push(new Token(TokenType::Equal, "==", 2));
+	tokenQueue.push(new Token(TokenType::Constant, "0", 2));
+	tokenQueue.push(new Token(TokenType::CloseParenthesis, ")", 2));
+	tokenQueue.push(new Token(TokenType::Or, "||", 2));
+	tokenQueue.push(new Token(TokenType::OpenParenthesis, "(", 2));
+	tokenQueue.push(new Token(TokenType::Identifier, "y", 2));
+	tokenQueue.push(new Token(TokenType::Equal, "==", 2));
+	tokenQueue.push(new Token(TokenType::Constant, "0", 2));
+	tokenQueue.push(new Token(TokenType::CloseParenthesis, ")", 2));
+	tokenQueue.push(new Token(TokenType::CloseParenthesis, ")", 2));
+	tokenQueue.push(new Token(TokenType::Separator, "{", 2));
+	tokenQueue.push(new Token(TokenType::Read, "read", 3));
+	tokenQueue.push(new Token(TokenType::Identifier, "x", 3));
+	tokenQueue.push(new Token(TokenType::Separator, ";", 3));
+	tokenQueue.push(new Token(TokenType::Separator, "}", 3));
+	tokenQueue.push(new Token(TokenType::Separator, "}", 3));
+	TNode* root = parser.buildAST(tokenQueue);
+	TNode* procTNode = root->getChildren().front();
+	TNode* stmtLst = procTNode->getChildren().front();
+	TNode* whileTNode = stmtLst->getChildren().front();
+	TNode* whileCondExprAnd = whileTNode->getChildren().front();
+	TNode* whileCondExpr1 = whileCondExprAnd->getChildren().front();
+	TNode* whileCondExpr2 = whileCondExprAnd->getChildren().back();
+	TNode* whileStmtLst = whileTNode->getChildren().back();
+	TNode* readTNode = whileStmtLst->getChildren().front();
+
+	SECTION("Root of AST has one StatementList child") {
+		REQUIRE(stmtLst->getType() == TNodeType::StatementList);
+	}
+
+	SECTION("StatementList has one While child") {
+		REQUIRE(whileTNode->getType() == TNodeType::While);
+	}
+
+	SECTION("While has ConditionExpression and StatementList children") {
+		REQUIRE(whileCondExprAnd->getType() == TNodeType::And);
+		REQUIRE(whileStmtLst->getType() == TNodeType::StatementList);
+	}
+
+	SECTION("Both ConditionExpressions has Variable and Constant children") {
+		TNode* left1 = whileCondExpr1->getChildren().front();
+		TNode* right1 = whileCondExpr1->getChildren().back();
+		REQUIRE(left1->getType() == TNodeType::Variable);
+		REQUIRE(left1->getName() == "x");
+		REQUIRE(right1->getType() == TNodeType::Constant);
+		REQUIRE(right1->getName() == "0");
+
+		TNode* left2 = whileCondExpr2->getChildren().front();
+		TNode* right2 = whileCondExpr2->getChildren().back();
+		REQUIRE(left2->getType() == TNodeType::Variable);
+		REQUIRE(left2->getName() == "y");
+		REQUIRE(right2->getType() == TNodeType::Constant);
+		REQUIRE(right2->getName() == "0");
+	}
+
+	SECTION("WhileStatementList has one Read child") {
+		REQUIRE(readTNode->getType() == TNodeType::Read);
+
+		TNode* variableTNode = readTNode->getChildren().front();
+		REQUIRE(variableTNode->getType() == TNodeType::Variable);
+		REQUIRE(variableTNode->getName() == "x");
+	}
+
+}
