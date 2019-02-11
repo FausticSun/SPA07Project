bafa1ff916b2bd2718084cc118fe2e1785c544a3
FausticSun <FausticSun@gmail.com>
2019-02-11 17:47:34 +0800

    Comment out error statements

--- src/spa/src/SPA.cpp
+++ src/spa/src/SPA.cpp
@@ -3,8 +3,9 @@
 
 void SPA::parseSIMPLEFile(std::string filename) { (void)filename; }
 const std::list<std::string> SPA::evaluateQuery(std::string query) const {
-  (void)query;
-  PqlEvaluator pe(pkb);
-  list<string> results = pe.evaluateQuery(query);
-  return results;
+  // (void)query;
+  // PqlEvaluator pe(pkb);
+  // list<string> results = pe.evaluateQuery(query);
+  //  return results;
+  return {};
 }
