53073fb2c6b1041d0ffc0d86828fbd18ad39289a
FausticSun <FausticSun@gmail.com>
2019-02-11 22:30:42 +0800

    Update SPA to store PKB in heap and parse SIMPLE logic

--- src/spa/src/SPA.cpp
+++ src/spa/src/SPA.cpp
@@ -1,7 +1,20 @@
 #include "SPA.h"
+#include "DesignExtractor.h"
+#include "Lexer.h"
+#include "Parser.h"
 #include "PqlEvaluator.h"
+#include <fstream>
 
-void SPA::parseSIMPLEFile(std::string filename) { (void)filename; }
+SPA::SPA() : pkb(new PKB()) {}
+
+void SPA::parseSIMPLEFile(std::string filename) {
+  Parser parser;
+  auto tokens = parser.parse(filename);
+  auto ast = parser.buildAst(tokens);
+  DesignExtractor designExtractor(ast);
+  std::unique_ptr<PKB> newPKB = designExtractor.getPKB();
+  pkb.swap(newPKB);
+}
 const std::list<std::string> SPA::evaluateQuery(std::string query) const {
   // (void)query;
   // PqlEvaluator pe(pkb);
--- src/spa/src/SPA.h
+++ src/spa/src/SPA.h
@@ -1,13 +1,15 @@
 #pragma once
 #include "PKB.h"
 #include <list>
+#include <memory>
 #include <string>
 
 class SPA {
 private:
-  PKB pkb;
+  std::unique_ptr<PKB> pkb;
 
 public:
+  SPA();
   void parseSIMPLEFile(std::string filename);
   const std::list<std::string> evaluateQuery(std::string query) const;
 };
