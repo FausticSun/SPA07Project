1562be324e38f17512e5e54d692c4beb506d1111
FausticSun <FausticSun@gmail.com>
2019-02-12 11:36:29 +0800

    Update PKB API

--- src/spa/src/PKB.h
+++ src/spa/src/PKB.h
@@ -1,32 +1,63 @@
 #pragma once
 
 #include "Relation.h"
+#include <datetimeapi.h>
 #include <map>
 #include <set>
 #include <string>
+
+enum class StatementType { Stmt, Assign, If, While, Read, Call, Print };
 
 class PKB {
 private:
   std::set<std::string> varTable;
   std::set<std::string> procTable;
-  std::map<Entity, std::set<Entity>> followsTable;
-  std::map<Entity, std::set<Entity>> followedByTable;
-  std::map<Entity, std::set<Entity>> parentTable;
-  std::map<Entity, std::set<Entity>> parentOfTable;
-  std::map<Entity, std::set<Entity>> usesTable;
-  std::map<Entity, std::set<Entity>> usedByTable;
-  std::map<Entity, std::set<Entity>> modifiesTable;
-  std::map<Entity, std::set<Entity>> modifiedByTable;
+  std::map<StatementType, std::set<std::string>> stmtTable;
+  std::map<std::string, std::set<std::string>> followsTable;
+  std::map<std::string, std::set<std::string>> followedByTable;
+  std::map<std::string, std::set<std::string>> followsStarTable;
+  std::map<std::string, std::set<std::string>> followedByStarTable;
+  std::map<std::string, std::set<std::string>> parentTable;
+  std::map<std::string, std::set<std::string>> parentOfTable;
+  std::map<std::string, std::set<std::string>> parentStarTable;
+  std::map<std::string, std::set<std::string>> parentOfStarTable;
+  std::map<std::string, std::set<std::string>> usesTable;
+  std::map<std::string, std::set<std::string>> usedByTable;
+  std::map<std::string, std::set<std::string>> modifiesTable;
+  std::map<std::string, std::set<std::string>> modifiedByTable;
+  std::map<std::string, std::string> assignTable;
 
 public:
   PKB();
   void insertVar(std::string);
   void insertProc(std::string);
-  void setFollows(Entity, Entity);
-  void setParent(Entity, Entity);
-  void setUsed(Entity, Entity);
-  void setModified(Entity, Entity);
+  void insertStatement(std::string, StatementType);
+  void setFollows(std::string, std::string);
+  void setParent(std::string, std::string);
+  void setUsed(std::string, std::string);
+  void setModified(std::string, std::string);
 
   const std::set<std::string> getVarTable() const;
   const std::set<std::string> getProcTable() const;
+  const std::set<std::string> getStatementsOfType(StatementType) const;
+  bool follows(std::string, std::string);
+  std::set<std::string> getFollows(std::string);
+  std::set<std::string> getFollowedBy(std::string);
+  bool followsT(std::string, std::string);
+  std::set<std::string> getFollowsT(std::string);
+  std::set<std::string> getFollowedByT(std::string);
+  bool parent(std::string, std::string);
+  std::set<std::string> getParent(std::string);
+  std::set<std::string> getParentOf(std::string);
+  bool parentT(std::string, std::string);
+  std::set<std::string> getParentT(std::string);
+  std::set<std::string> getParentOfT(std::string);
+  bool uses(std::string, std::string);
+  std::set<std::string> getUses(std::string);
+  std::set<std::string> getUsedBy(std::string);
+  bool modifies(std::string, std::string);
+  std::set<std::string> getModifies(std::string);
+  std::set<std::string> getModifiedBy(std::string);
+  bool matchAssign(std::string stmtNo, std::string expr, bool partial);
+  std::set<std::string> getAssignMatches(std::string expr, bool partial);
 };
