649ed89306d383f2c657666487929ff91544161b
elroyhaw <elroyhaw@gmail.com>
2019-02-14 09:38:17 +0800

    Develop Uses and Modifies base facts extractor API

--- src/spa/src/DesignExtractor.h
+++ src/spa/src/DesignExtractor.h
@@ -5,8 +5,9 @@
 class DesignExtractor {
 private:
   std::unique_ptr<PKB> pkb;
-  void traverseAST(std::unique_ptr<TNode> &AST, std::vector<int> parents = {});
-  void handleStmtLst(std::unique_ptr<TNode> &AST);
+  void traverseAST(std::unique_ptr<TNode> &AST);
+  void extractUses(std::unique_ptr<TNode> &AST, int parent);
+  void extractModifies(std::unique_ptr<TNode> &AST, int parent);
 
 public:
   DesignExtractor(std::unique_ptr<TNode> &AST);
