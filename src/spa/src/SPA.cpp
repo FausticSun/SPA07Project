234629c907fe71c7625e506eefb5bf9c30e0b112
FausticSun <FausticSun@gmail.com>
2019-02-26 14:30:56 +0800

    Handle SIMPLE parsing exception

--- src/spa/src/SPA.cpp
+++ src/spa/src/SPA.cpp
@@ -13,27 +13,31 @@ SPA::SPA() : pkb(new PKB()) {}
 void SPA::parseSIMPLEFile(std::string filename) {
   Parser parser;
   Lexer lexer;
-  std::ifstream file;
-  file.open(filename);
-  std::istream &fileStream = file;
-  auto tokens = lexer.tokenizeFile(fileStream);
-  auto ast = parser.buildAst(tokens);
-  DesignExtractor designExtractor(ast);
-  std::unique_ptr<PKB> newPKB = designExtractor.getPKB();
-  pkb.swap(newPKB);
+  try {
+    std::ifstream file;
+    file.open(filename);
+    std::istream &fileStream = file;
+    auto tokens = lexer.tokenizeFile(fileStream);
+    auto ast = parser.buildAst(tokens);
+    DesignExtractor designExtractor(ast);
+    std::unique_ptr<PKB> newPKB = designExtractor.getPKB();
+    pkb.swap(newPKB);
+  } catch (std::logic_error e) {
+    exit(0);
+  }
 }
 const std::list<std::string> SPA::evaluateQuery(std::string queryString) const {
-	list<string> results;
+  list<string> results;
   try {
-	  PQLParser pqlParser;
-	  auto tokens = pqlParser.parse(queryString);
-	  auto query = pqlParser.buildQuery(tokens);
-	  PqlEvaluator pe(*pkb);
-	  auto results = pe.executeQuery(query);
-	  return results;
-  }catch (logic_error le) {
-	  return results;
-  }catch(invalid_argument ia) {
-	  return results;
+    PQLParser pqlParser;
+    auto tokens = pqlParser.parse(queryString);
+    auto query = pqlParser.buildQuery(tokens);
+    PqlEvaluator pe(*pkb);
+    auto results = pe.executeQuery(query);
+    return results;
+  } catch (logic_error le) {
+    return results;
+  } catch (invalid_argument ia) {
+    return results;
   }
 }
