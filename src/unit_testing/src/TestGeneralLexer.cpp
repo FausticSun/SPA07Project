dbf86f29ce3b1b7b83c58830739061704cd2afa2
FausticSun <FausticSun@gmail.com>
2019-03-01 11:51:01 +0800

    Fix general lexer unit tests

--- src/unit_testing/src/TestGeneralLexer.cpp
+++ src/unit_testing/src/TestGeneralLexer.cpp
@@ -5,7 +5,7 @@
 SCENARIO("Single alphabetic identifier") {
   std::stringstream ss;
   ss << "Test" << '\n';
-  auto tokens = GeneralLexer::tokenize(ss);
+  auto tokens = Lexer::tokenize(ss);
   REQUIRE(tokens.begin()->type == TokenType::Identifier);
   REQUIRE(tokens.begin()->value == "Test");
 }
@@ -13,7 +13,7 @@ SCENARIO("Single alphabetic identifier") {
 SCENARIO("Single alphanumeric identifier") {
   std::stringstream ss;
   ss << "Test123" << '\n';
-  auto tokens = GeneralLexer::tokenize(ss);
+  auto tokens = Lexer::tokenize(ss);
   REQUIRE(tokens.begin()->type == TokenType::Identifier);
   REQUIRE(tokens.begin()->value == "Test123");
 }
@@ -21,7 +21,7 @@ SCENARIO("Single alphanumeric identifier") {
 SCENARIO("Single number") {
   std::stringstream ss;
   ss << "12345" << '\n';
-  auto tokens = GeneralLexer::tokenize(ss);
+  auto tokens = Lexer::tokenize(ss);
   REQUIRE(tokens.begin()->type == TokenType::Number);
   REQUIRE(tokens.begin()->value == "12345");
 }
@@ -29,20 +29,20 @@ SCENARIO("Single number") {
 SCENARIO("Invalid identifier") {
   std::stringstream ss;
   ss << "123Test" << '\n';
-  REQUIRE_THROWS(GeneralLexer::tokenize(ss));
+  REQUIRE_THROWS(Lexer::tokenize(ss));
 }
 
 SCENARIO("Consume comment with newline") {
   std::stringstream ss;
   ss << "// This is a comment" << '\n';
-  auto tokens = GeneralLexer::tokenize(ss);
+  auto tokens = Lexer::tokenize(ss);
   REQUIRE(tokens.begin()->type == TokenType::Whitespace);
 }
 
 SCENARIO("Consume comment without newline") {
   std::stringstream ss;
   ss << "// This is a comment";
-  auto tokens = GeneralLexer::tokenize(ss);
+  auto tokens = Lexer::tokenize(ss);
   REQUIRE(tokens.empty());
 }
 
@@ -50,7 +50,7 @@ SCENARIO("Many spaces") {
   std::stringstream ss;
   ss << "              "
      << "\n";
-  auto tokens = GeneralLexer::tokenize(ss);
+  auto tokens = Lexer::tokenize(ss);
   REQUIRE(tokens.size() == 1);
   REQUIRE(tokens.begin()->type == TokenType::Whitespace);
 }
@@ -58,7 +58,7 @@ SCENARIO("Many spaces") {
 SCENARIO("Operators") {
   std::stringstream ss;
   ss << "=+-*/%_><!&&||>=<=!===";
-  auto tokens = GeneralLexer::tokenize(ss);
+  auto tokens = Lexer::tokenize(ss);
   REQUIRE(tokens.size() == 16);
   for (auto it = tokens.begin(); it != tokens.end(); ++it) {
     REQUIRE(it->type == TokenType::Operator);
@@ -85,7 +85,7 @@ SCENARIO("Operators") {
 SCENARIO("Delimiters") {
   std::stringstream ss;
   ss << "{(\"#.,;\")}";
-  auto tokens = GeneralLexer::tokenize(ss);
+  auto tokens = Lexer::tokenize(ss);
   REQUIRE(tokens.size() == 10);
   for (auto it = tokens.begin(); it != tokens.end(); ++it) {
     REQUIRE(it->type == TokenType::Delimiter);
