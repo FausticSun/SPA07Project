4161ad4ff90cf7b7952ae8ab79647ed80b5093ea
nitro <zpyfengyingwu@gmail.com>
2019-03-13 18:02:10 +0800

    update PqlEvaluator.

--- src/spa/src/PQLEvaluator.h
+++ src/spa/src/PQLEvaluator.h
@@ -5,22 +5,23 @@
 #include <list>
 #include <string>
 #include <vector>
+#include <Table.h>
 
 using namespace std;
 
-class ClauseResult {
-public:
-  ClauseResult(bool isbool, bool bvalue, vector<QueryEntity> titles = {},
-               vector<vector<string>> resultTable = {})
-      : isBool(isbool), bValue(bvalue), titles(titles),
-        resultTable(resultTable){};
-  bool isBool;
-  bool bValue;
-  vector<QueryEntity> titles;
-  vector<vector<string>> resultTable;
-  void normalize();
-  bool contains(QueryEntity &q);
-};
+//class ClauseResult {
+//public:
+//  ClauseResult(bool isbool, bool bvalue, vector<QueryEntity> titles = {},
+//               vector<vector<string>> resultTable = {})
+//      : isBool(isbool), bValue(bvalue), titles(titles),
+//        resultTable(resultTable){};
+//  bool isBool;
+//  bool bValue;
+//  vector<QueryEntity> titles;
+//  vector<vector<string>> resultTable;
+//  void normalize();
+//  bool contains(QueryEntity &q);
+//};
 
 class PqlEvaluator {
 public:
@@ -30,26 +31,25 @@ public:
 
 private:
   PKB mypkb;
-  list<string> executeSimpleQuery(QueryEntityType q);
-  ClauseResult getModifies(Clause c);
-  ClauseResult getUses(Clause c);
-  ClauseResult getParent(Clause c);
-  ClauseResult getParentS(Clause c);
-  ClauseResult getFollows(Clause c);
-  ClauseResult getFollowsS(Clause c);
-  ClauseResult getAssPatern(Clause c);
-  set<string> getdataByTtype(QueryEntityType q);
+  list<string> executeSimpleQuery(vector<QueryEntity> t);
+  Table getdataByTtype(QueryEntityType q);
   bool isSynonym(QueryEntityType q);
   bool isUnderscore(QueryEntityType q);
   bool isConstant(QueryEntityType q);
-  bool validateType(string result, QueryEntityType q);
-  bool validateStmt(string result, QueryEntityType q);
   bool isVar(string result, QueryEntityType q);
   bool isPro(string result, QueryEntityType q);
-  bool isCons(string result, QueryEntityType q);
+
   StatementType convertQType(QueryEntityType q);
 
-  /*map<string, QueryEntityType> executeDeclaration(const vector<QueryEntity>
-  &selectors); vector<ClauseResult> excuteClauses(const vector<Clause>
-  &clauses); int analyseClauseConstants(const Clause &clause);*/
+  //ClauseResult getModifies(Clause c);
+//ClauseResult getUses(Clause c);
+//ClauseResult getParent(Clause c);
+//ClauseResult getParentS(Clause c);
+//ClauseResult getFollows(Clause c);
+//ClauseResult getFollowsS(Clause c);
+//ClauseResult getAssPatern(Clause c);
+
+	//bool validateType(string result, QueryEntityType q);
+  //bool validateStmt(string result, QueryEntityType q);
+  //bool isCons(string result, QueryEntityType q);
 };
