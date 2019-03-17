5c76c5192c70e2edf5bd8ce0dfb51ceb71971e82
elroyhaw <elroyhaw@gmail.com>
2019-03-17 14:05:22 +0800

    Implement populateCFG in DesignExtractor

--- src/spa/src/DesignExtractor.cpp
+++ src/spa/src/DesignExtractor.cpp
@@ -203,7 +203,24 @@ void populateUsesAndModifiesS(std::unique_ptr<PKB> &pkb) {
   }
 }
 
-void populateCFG(std::unique_ptr<PKB> &pkb) {}
+void populateCFG(std::unique_ptr<PKB> &pkb) {
+  // Get first line number of each procedure
+  auto procStmtTable = pkb->getProcStmt();
+  procStmtTable.setHeader({"proc", "n1"});
+  auto nextTable = pkb->getNext();
+  nextTable.setHeader({"n1", "n2"});
+  procStmtTable.mergeWith(nextTable);
+
+  // Get all while and if line numbers
+  auto whileIfTable = pkb->getStmtType(StatementType::While);
+  auto ifTable = pkb->getStmtType(StatementType::If);
+  whileIfTable.concatenate(ifTable);
+
+  for (auto data : procStmtTable.getData({"proc", "n1"})) {
+    CFG graph = CFG{std::stoi(data[1]), pkb->getNext(), whileIfTable};
+    pkb->setCFG(data[0], graph);
+  }
+}
 
 void DesignExtractor::populateDesigns(std::unique_ptr<PKB> &pkb) {
   validateProcs(pkb);
