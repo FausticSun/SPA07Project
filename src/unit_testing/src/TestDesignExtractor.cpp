201b69190d528eb91c585eed74b4784bd893357e
elroyhaw <elroyhaw@gmail.com>
2019-03-18 23:34:57 +0800

    Update unit tests for DesignExtractor

--- src/unit_testing/src/TestDesignExtractor.cpp
+++ src/unit_testing/src/TestDesignExtractor.cpp
@@ -3,7 +3,7 @@
 #include "catch.hpp"
 #include <sstream>
 
-using Catch::Matchers::Contains;
+using Catch::Matchers::UnorderedEquals;
 
 TEST_CASE("Validate call to non-existant proc") {
   std::unique_ptr<PKB> pkb{new PKB()};
@@ -61,6 +61,10 @@ TEST_CASE("Populate ParentT") {
 
 TEST_CASE("Populate CallsT") {
   std::unique_ptr<PKB> pkb{new PKB()};
+  pkb->setProc("A", 1, 2);
+  pkb->setProc("B", 2, 3);
+  pkb->setProc("C", 3, 4);
+  pkb->setProc("D", 4, 5);
   pkb->setCalls("A", "B");
   pkb->setCalls("B", "C");
   pkb->setCalls("C", "D");
@@ -82,7 +86,10 @@ TEST_CASE("Populate UsesS") {
   pkb->setUses(3, "c");
   pkb->setUses(4, "d");
   pkb->setParent(1, 2);
+  pkb->setParent(1, 3);
   pkb->setParent(3, 4);
+  pkb->setStmtType(1, StatementType::While);
+  pkb->setStmtType(3, StatementType::While);
   DesignExtractor::populateDesigns(pkb);
   auto usesSTable = pkb->getUsesS();
   REQUIRE(usesSTable.size() == 8);
@@ -101,7 +108,10 @@ TEST_CASE("Populate ModifiesS") {
   pkb->setModifies(2, "a");
   pkb->setModifies(4, "b");
   pkb->setParent(1, 2);
+  pkb->setParent(1, 3);
   pkb->setParent(3, 4);
+  pkb->setStmtType(1, StatementType::While);
+  pkb->setStmtType(3, StatementType::While);
   DesignExtractor::populateDesigns(pkb);
   auto modifiesSTable = pkb->getModifiesS();
   REQUIRE(modifiesSTable.size() == 5);
@@ -114,26 +124,39 @@ TEST_CASE("Populate ModifiesS") {
 
 TEST_CASE("Populate Use and Modifies for Call statements") {
   std::unique_ptr<PKB> pkb{new PKB()};
+  pkb->setProc("A", 1, 2);
+  pkb->setProc("B", 2, 4);
   pkb->setCalls("A", "B");
   pkb->setCallProcName(1, "B");
   pkb->setUses("B", "a");
   pkb->setUses("B", "b");
+  pkb->setUses(2, "a");
+  pkb->setUses(3, "b");
   pkb->setModifies("B", "c");
   pkb->setModifies("B", "d");
+  pkb->setModifies(2, "c");
+  pkb->setModifies(3, "d");
   DesignExtractor::populateDesigns(pkb);
   auto usesPTable = pkb->getUsesP();
   auto usesSTable = pkb->getUsesS();
   auto modifiesPTable = pkb->getModifiesP();
   auto modifiesSTable = pkb->getModifiesS();
-  REQUIRE(usesPTable.size() == 5);
+  REQUIRE(usesPTable.size() == 4);
+  REQUIRE(usesSTable.size() == 4);
+  REQUIRE(modifiesPTable.size() == 4);
+  REQUIRE(modifiesSTable.size() == 4);
   REQUIRE(usesPTable.contains({"A", "a"}));
   REQUIRE(usesPTable.contains({"A", "b"}));
   REQUIRE(usesSTable.contains({"1", "a"}));
   REQUIRE(usesSTable.contains({"1", "b"}));
+  REQUIRE(usesSTable.contains({"2", "a"}));
+  REQUIRE(usesSTable.contains({"3", "b"}));
   REQUIRE(modifiesPTable.contains({"A", "c"}));
   REQUIRE(modifiesPTable.contains({"A", "d"}));
   REQUIRE(modifiesSTable.contains({"1", "c"}));
   REQUIRE(modifiesSTable.contains({"1", "d"}));
+  REQUIRE(modifiesSTable.contains({"2", "c"}));
+  REQUIRE(modifiesSTable.contains({"3", "d"}));
 }
 
 TEST_CASE("Populate CFG") {
@@ -171,21 +194,20 @@ TEST_CASE("Populate CFG") {
     auto CFG = pkb->getCFG("A");
     auto forwardCFG = CFG.getForwardCompressedGraph();
     auto reverseCFG = CFG.getReverseCompressedGraph();
-    std::vector<int> empty;
     REQUIRE(forwardCFG.size() == 6);
     REQUIRE(reverseCFG.size() == 6);
-    REQUIRE_THAT(forwardCFG[0], Contains({1, 2}));
-    REQUIRE_THAT(forwardCFG[1], Contains({0}));
-    REQUIRE_THAT(forwardCFG[2], Contains({3, 4}));
-    REQUIRE_THAT(forwardCFG[3], Contains({5}));
-    REQUIRE_THAT(forwardCFG[4], Contains({5}));
-    REQUIRE_THAT(forwardCFG[5], Contains(empty));
-    REQUIRE_THAT(reverseCFG[0], Contains({1}));
-    REQUIRE_THAT(reverseCFG[1], Contains({0}));
-    REQUIRE_THAT(reverseCFG[2], Contains({0}));
-    REQUIRE_THAT(reverseCFG[3], Contains({2}));
-    REQUIRE_THAT(reverseCFG[4], Contains({2}));
-    REQUIRE_THAT(reverseCFG[5], Contains({3, 4}));
+    REQUIRE_THAT(forwardCFG[0], UnorderedEquals(std::vector<int>{1, 2}));
+    REQUIRE_THAT(forwardCFG[1], UnorderedEquals(std::vector<int>{0}));
+    REQUIRE_THAT(forwardCFG[2], UnorderedEquals(std::vector<int>{3, 4}));
+    REQUIRE_THAT(forwardCFG[3], UnorderedEquals(std::vector<int>{5}));
+    REQUIRE_THAT(forwardCFG[4], UnorderedEquals(std::vector<int>{5}));
+    REQUIRE_THAT(forwardCFG[5], UnorderedEquals(std::vector<int>{}));
+    REQUIRE_THAT(reverseCFG[0], UnorderedEquals(std::vector<int>{1}));
+    REQUIRE_THAT(reverseCFG[1], UnorderedEquals(std::vector<int>{0}));
+    REQUIRE_THAT(reverseCFG[2], UnorderedEquals(std::vector<int>{1}));
+    REQUIRE_THAT(reverseCFG[3], UnorderedEquals(std::vector<int>{2}));
+    REQUIRE_THAT(reverseCFG[4], UnorderedEquals(std::vector<int>{2}));
+    REQUIRE_THAT(reverseCFG[5], UnorderedEquals(std::vector<int>{3, 4}));
   }
 
   SECTION("CFG with two if statements nested") {
@@ -212,23 +234,22 @@ TEST_CASE("Populate CFG") {
     auto CFG = pkb->getCFG("A");
     auto forwardCFG = CFG.getForwardCompressedGraph();
     auto reverseCFG = CFG.getReverseCompressedGraph();
-    std::vector<int> empty;
     REQUIRE(forwardCFG.size() == 7);
     REQUIRE(reverseCFG.size() == 7);
-    REQUIRE_THAT(forwardCFG[0], Contains({1}));
-    REQUIRE_THAT(forwardCFG[1], Contains({2, 3}));
-    REQUIRE_THAT(forwardCFG[2], Contains({4, 5}));
-    REQUIRE_THAT(forwardCFG[3], Contains({6}));
-    REQUIRE_THAT(forwardCFG[4], Contains({6}));
-    REQUIRE_THAT(forwardCFG[5], Contains({6}));
-    REQUIRE_THAT(forwardCFG[6], Contains(empty));
-    REQUIRE_THAT(reverseCFG[0], Contains(empty));
-    REQUIRE_THAT(reverseCFG[1], Contains({0}));
-    REQUIRE_THAT(reverseCFG[2], Contains({1}));
-    REQUIRE_THAT(reverseCFG[3], Contains({1}));
-    REQUIRE_THAT(reverseCFG[4], Contains({2}));
-    REQUIRE_THAT(reverseCFG[5], Contains({2}));
-    REQUIRE_THAT(reverseCFG[6], Contains({3, 4, 5}));
+    REQUIRE_THAT(forwardCFG[0], UnorderedEquals(std::vector<int>{1}));
+    REQUIRE_THAT(forwardCFG[1], UnorderedEquals(std::vector<int>{2, 3}));
+    REQUIRE_THAT(forwardCFG[2], UnorderedEquals(std::vector<int>{5, 6}));
+    REQUIRE_THAT(forwardCFG[3], UnorderedEquals(std::vector<int>{4}));
+    REQUIRE_THAT(forwardCFG[4], UnorderedEquals(std::vector<int>{}));
+    REQUIRE_THAT(forwardCFG[5], UnorderedEquals(std::vector<int>{4}));
+    REQUIRE_THAT(forwardCFG[6], UnorderedEquals(std::vector<int>{4}));
+    REQUIRE_THAT(reverseCFG[0], UnorderedEquals(std::vector<int>{}));
+    REQUIRE_THAT(reverseCFG[1], UnorderedEquals(std::vector<int>{0}));
+    REQUIRE_THAT(reverseCFG[2], UnorderedEquals(std::vector<int>{1}));
+    REQUIRE_THAT(reverseCFG[3], UnorderedEquals(std::vector<int>{1}));
+    REQUIRE_THAT(reverseCFG[4], UnorderedEquals(std::vector<int>{3, 5, 6}));
+    REQUIRE_THAT(reverseCFG[5], UnorderedEquals(std::vector<int>{2}));
+    REQUIRE_THAT(reverseCFG[6], UnorderedEquals(std::vector<int>{2}));
   }
 
   SECTION("CFG with two while statements nested") {
@@ -246,17 +267,16 @@ TEST_CASE("Populate CFG") {
     auto CFG = pkb->getCFG("A");
     auto forwardCFG = CFG.getForwardCompressedGraph();
     auto reverseCFG = CFG.getReverseCompressedGraph();
-    std::vector<int> empty;
     REQUIRE(forwardCFG.size() == 4);
     REQUIRE(reverseCFG.size() == 4);
-    REQUIRE_THAT(forwardCFG[0], Contains({1, 2}));
-    REQUIRE_THAT(forwardCFG[1], Contains({0, 3}));
-    REQUIRE_THAT(forwardCFG[2], Contains(empty));
-    REQUIRE_THAT(forwardCFG[3], Contains({1}));
-    REQUIRE_THAT(reverseCFG[0], Contains({1}));
-    REQUIRE_THAT(reverseCFG[1], Contains({0, 3}));
-    REQUIRE_THAT(reverseCFG[2], Contains({0}));
-    REQUIRE_THAT(reverseCFG[3], Contains({2}));
+    REQUIRE_THAT(forwardCFG[0], UnorderedEquals(std::vector<int>{1, 2}));
+    REQUIRE_THAT(forwardCFG[1], UnorderedEquals(std::vector<int>{0, 3}));
+    REQUIRE_THAT(forwardCFG[2], UnorderedEquals(std::vector<int>{}));
+    REQUIRE_THAT(forwardCFG[3], UnorderedEquals(std::vector<int>{1}));
+    REQUIRE_THAT(reverseCFG[0], UnorderedEquals(std::vector<int>{1}));
+    REQUIRE_THAT(reverseCFG[1], UnorderedEquals(std::vector<int>{0, 3}));
+    REQUIRE_THAT(reverseCFG[2], UnorderedEquals(std::vector<int>{0}));
+    REQUIRE_THAT(reverseCFG[3], UnorderedEquals(std::vector<int>{2}));
   }
 
   SECTION("CFG with while nested in if statement") {
@@ -264,11 +284,11 @@ TEST_CASE("Populate CFG") {
     pkb->setProc("A", 1, 8);
     pkb->setNext(1, 2);
     pkb->setNext(1, 3);
-    pkb->setNext(2, 6);
+    pkb->setNext(2, 5);
     pkb->setNext(3, 4);
     pkb->setNext(4, 3);
-    pkb->setNext(3, 6);
-    pkb->setNext(6, 7);
+    pkb->setNext(3, 5);
+    pkb->setNext(5, 6);
     pkb->setStmtType(1, StatementType::If);
     pkb->setStmtType(3, StatementType::While);
     DesignExtractor::populateDesigns(pkb);
@@ -278,20 +298,20 @@ TEST_CASE("Populate CFG") {
     std::vector<int> empty;
     REQUIRE(forwardCFG.size() == 5);
     REQUIRE(reverseCFG.size() == 5);
-    REQUIRE_THAT(forwardCFG[0], Contains({1, 2}));
-    REQUIRE_THAT(forwardCFG[1], Contains({4}));
-    REQUIRE_THAT(forwardCFG[2], Contains({3, 4}));
-    REQUIRE_THAT(forwardCFG[3], Contains({2}));
-    REQUIRE_THAT(forwardCFG[4], Contains(empty));
-    REQUIRE_THAT(reverseCFG[0], Contains(empty));
-    REQUIRE_THAT(reverseCFG[1], Contains({0}));
-    REQUIRE_THAT(reverseCFG[2], Contains({0, 3}));
-    REQUIRE_THAT(reverseCFG[3], Contains({2}));
-    REQUIRE_THAT(reverseCFG[4], Contains({1, 2}));
+    REQUIRE_THAT(forwardCFG[0], UnorderedEquals(std::vector<int>{1, 2}));
+    REQUIRE_THAT(forwardCFG[1], UnorderedEquals(std::vector<int>{4}));
+    REQUIRE_THAT(forwardCFG[2], UnorderedEquals(std::vector<int>{3, 4}));
+    REQUIRE_THAT(forwardCFG[3], UnorderedEquals(std::vector<int>{2}));
+    REQUIRE_THAT(forwardCFG[4], UnorderedEquals(std::vector<int>{}));
+    REQUIRE_THAT(reverseCFG[0], UnorderedEquals(std::vector<int>{}));
+    REQUIRE_THAT(reverseCFG[1], UnorderedEquals(std::vector<int>{0}));
+    REQUIRE_THAT(reverseCFG[2], UnorderedEquals(std::vector<int>{0, 3}));
+    REQUIRE_THAT(reverseCFG[3], UnorderedEquals(std::vector<int>{2}));
+    REQUIRE_THAT(reverseCFG[4], UnorderedEquals(std::vector<int>{1, 2}));
   }
   SECTION("CFG with if nested in while statement") {
     std::unique_ptr<PKB> pkb{new PKB()};
-    pkb->setProc("A", 1, 8);
+    pkb->setProc("A", 1, 9);
     pkb->setNext(1, 2);
     pkb->setNext(2, 3);
     pkb->setNext(3, 4);
@@ -312,18 +332,18 @@ TEST_CASE("Populate CFG") {
     std::vector<int> empty;
     REQUIRE(forwardCFG.size() == 6);
     REQUIRE(reverseCFG.size() == 6);
-    REQUIRE_THAT(forwardCFG[0], Contains({1}));
-    REQUIRE_THAT(forwardCFG[1], Contains({2, 3}));
-    REQUIRE_THAT(forwardCFG[2], Contains({4, 5}));
-    REQUIRE_THAT(forwardCFG[3], Contains(empty));
-    REQUIRE_THAT(forwardCFG[4], Contains({3}));
-    REQUIRE_THAT(forwardCFG[5], Contains({3}));
-    REQUIRE_THAT(reverseCFG[0], Contains(empty));
-    REQUIRE_THAT(reverseCFG[1], Contains({0, 4, 5}));
-    REQUIRE_THAT(reverseCFG[2], Contains({3}));
-    REQUIRE_THAT(reverseCFG[3], Contains({1}));
-    REQUIRE_THAT(reverseCFG[4], Contains({2}));
-    REQUIRE_THAT(reverseCFG[5], Contains({2}));
+    REQUIRE_THAT(forwardCFG[0], UnorderedEquals(std::vector<int>{1}));
+    REQUIRE_THAT(forwardCFG[1], UnorderedEquals(std::vector<int>{2, 3}));
+    REQUIRE_THAT(forwardCFG[2], UnorderedEquals(std::vector<int>{4, 5}));
+    REQUIRE_THAT(forwardCFG[3], UnorderedEquals(std::vector<int>{}));
+    REQUIRE_THAT(forwardCFG[4], UnorderedEquals(std::vector<int>{3}));
+    REQUIRE_THAT(forwardCFG[5], UnorderedEquals(std::vector<int>{3}));
+    REQUIRE_THAT(reverseCFG[0], UnorderedEquals(std::vector<int>{}));
+    REQUIRE_THAT(reverseCFG[1], UnorderedEquals(std::vector<int>{0, 4, 5}));
+    REQUIRE_THAT(reverseCFG[2], UnorderedEquals(std::vector<int>{3}));
+    REQUIRE_THAT(reverseCFG[3], UnorderedEquals(std::vector<int>{1}));
+    REQUIRE_THAT(reverseCFG[4], UnorderedEquals(std::vector<int>{2}));
+    REQUIRE_THAT(reverseCFG[5], UnorderedEquals(std::vector<int>{2}));
   }
 
   SECTION("Multiple CFGs with multiple procedures") {
