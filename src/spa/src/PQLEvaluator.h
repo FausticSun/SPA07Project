51619ec4d7da13ed713d0f84c7327edf4654e73e
nitro <zpyfengyingwu@gmail.com>
2019-04-04 11:25:31 +0800

    add Affects

--- src/spa/src/PQLEvaluator.h
+++ src/spa/src/PQLEvaluator.h
@@ -40,6 +40,7 @@ private:
   ClauseResult dataFilter(Table data, Clause c);
   ClauseResult withEvaluate(Clause c);
 	ClauseResult NextTEvaluate(Clause clause);
+	ClauseResult AffectEvaluate(Clause clause);
   StatementType convertQType(QueryEntityType q);
   bool isSynonym(QueryEntityType q);
   bool isUnderscore(QueryEntityType q);
